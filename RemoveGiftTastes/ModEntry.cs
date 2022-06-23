6723831e1f70e246964bcb282e24d41a3b60e2fc
elizabethcd <55162529+elizabethcd@users.noreply.github.com>
2022-06-23 10:00:03 -0400

    Second initial commit

--- RemoveGiftTastes/ModEntry.cs
+++ RemoveGiftTastes/ModEntry.cs
@@ -1,10 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
 namespace RemoveGiftTastes
 {
-    public class ModEntry
+    /// <summary>The mod entry point.</summary>
+    public class ModEntry : Mod
     {
-        public ModEntry()
+        // Properties
+        public const string modContentPath = "Mods/vl.rgt/GiftTastesToRemove";
+
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
         {
+            // Add asset editor
+            helper.Events.Content.AssetRequested += this.OnAssetRequested;
+        }
+
+
+        /*********
+        ** Private methods
+        *********/
+        /// <summary>Raised after an asset is requested.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        private void OnAssetRequested(object sender, AssetRequestedEventArgs e)
+        {
+            if (e.NameWithoutLocale.IsEquivalentTo(PathUtilities.NormalizeAssetName("Data/NPCDispositions")))
+            {
+                Dictionary<string, string> removeDict = Helper.GameContent.Load<Dictionary<string, string>>(ModEntry.modContentPath);
+            }
+            else if(e.NameWithoutLocale.IsEquivalentTo(ModEntry.modContentPath))
+            {
+                e.LoadFrom(() => new Dictionary<string, string>(), AssetLoadPriority.Low);
+            }
         }
     }
 }
